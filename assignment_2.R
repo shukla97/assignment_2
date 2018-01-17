93713b30d5f5311ea6433d492f5d99519395a9c6
shukla97 <shubhamshkl270@gmail.com>
2018-01-17 21:30:36 +0530

    Forth commit

--- assignment_2.R
+++ assignment_2.R
@@ -1,8 +1,6 @@
 # See README.md for instructions on running the code and output from it
-# The assignment states that running the code is not part of the grading 
-# but I have the instructions anyway.
 
-# makeCacheMatrix is a function that returns a list of functions
+# make_cache_matrix is a function that returns a list of functions
 # Its puspose is to store a martix and a cached value of the inverse of the 
 # matrix. Contains the following functions:
 # * set_matrix      set the value of a matrix
