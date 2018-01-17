3cbfa9c31818d1a48a268441c788f6f1d05ebd2e
shukla97 <shubhamshkl270@gmail.com>
2018-01-17 21:26:35 +0530

    Second commit

--- assignment_2.R
+++ assignment_2.R
@@ -5,10 +5,10 @@
 # makeCacheMatrix is a function that returns a list of functions
 # Its puspose is to store a martix and a cached value of the inverse of the 
 # matrix. Contains the following functions:
-# * setMatrix      set the value of a matrix
-# * getMatrix      get the value of a matrix
-# * cacheInverse   get the cahced value (inverse of the matrix)
-# * getInverse     get the cahced value (inverse of the matrix)
+# * set_matrix      set the value of a matrix
+# * get_matrix      get the value of a matrix
+# * cache_inverse   get the cahced value (inverse of the matrix)
+# * get_inverse     get the cahced value (inverse of the matrix)
 #
 # Notes:
 # not sure how the "x = numeric()" part works in the argument list of the 
